37061bd59d8e61c350479cb011729cf3b3f11bef
Art-Net <53600019+4rt-Net@users.noreply.github.com>
2023-10-24 10:02:50 +0200

    Update Curl_Fuzzer.sh

--- Curl_Fuzzer.sh
+++ Curl_Fuzzer.sh
@@ -1,27 +1,19 @@
 #!/bin/bash
 
-# URL to check directories against
-BASE_URL="http://example.com/"
+BASE_URL="http://example.com/" #Change URL as needed, remember to include trailing '/'
 
-# Output file to store the found directories
-OUTPUT_FILE="found_directories.txt"
+OUTPUT_FILE="found_directories.txt" #Change output filename as needed. 
 
-# Remove the output file if it already exists
 [ -e "$OUTPUT_FILE" ] && rm "$OUTPUT_FILE"
 
-# Read each line from Directories.txt and check if the directory exists
 while IFS= read -r directory; do
-    # Use curl to check if the directory exists (HTTP status code 200 indicates success)
     response=$(curl -s -o /dev/null -w "%{http_code}" "${BASE_URL}${directory}")
 
-    # If the directory exists, output it and write to the output file
     if [ "$response" -eq 200 ]; then
         echo "Directory '$directory' exists on ${BASE_URL}"
         echo "$directory" >> "$OUTPUT_FILE"
     fi
-    
-    # Add a 2-second delay before the next request
-    sleep 2
-done < path_to_directories_wordlist
+    sleep 2 #Delay between requests set to 2 seconds by default. 
+done < path_to_directories_wordlist #Change path to wordlist file as needed.
 
 echo "Found directories written to $OUTPUT_FILE"
