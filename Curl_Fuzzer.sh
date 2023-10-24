adb63c7e8258fde1c754fe5187f14639ed6aa6c3
Art-Net <53600019+4rt-Net@users.noreply.github.com>
2023-10-24 13:00:25 +0200

    Update Curl_Fuzzer.sh

--- Curl_Fuzzer.sh
+++ Curl_Fuzzer.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-#Change url as needed, remember to include trailing '/' depending on wordlist. 
+# Change URL as needed
 BASE_URL="http://example.com/"
 
-#Change wordlist path as needed
+# Change path to input file and wordlist as needed
 INPUT_FILE="Directories.txt"
 
-#Change output filename as needed.
+# Change output filename as needed
 OUTPUT_FILE="found_directories.txt"
 
 TOTAL_LINES=$(wc -l < "$INPUT_FILE")
@@ -16,19 +16,19 @@ CURRENT_LINE=0
 [ -e "$OUTPUT_FILE" ] && rm "$OUTPUT_FILE"
 
 while IFS= read -r directory; do
-    # Increment the current line count
     CURRENT_LINE=$((CURRENT_LINE + 1))
     
-    echo "$CURRENT_LINE/$TOTAL_LINES - ${BASE_URL}${directory}"
-    
+    echo -n "$CURRENT_LINE/$TOTAL_LINES - ${BASE_URL}${directory} "
+
     response=$(curl -s -o /dev/null -w "%{http_code}" "${BASE_URL}${directory}")
 
     if [ "$response" -eq 200 ]; then
-        echo "Directory '$directory' exists on ${BASE_URL}"
+        echo "Found!"
         echo "$directory" >> "$OUTPUT_FILE"
+    else
+        echo "Not Found"
     fi
-    
-    # Change delay as needed depending on network sensitivity
+    # Time delay added in case of network sensitivity. Change this value or comment out sleep delay depending on requirements. 
     sleep 2
 done < "$INPUT_FILE"
 
